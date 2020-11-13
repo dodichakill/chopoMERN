138ed4d139a16e9b43127f5be0a219201b0c20b7
dodichakill <dodiherlangga91@gmail.com>
2020-11-13 03:20:42 -0800

    clr ep3.3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "assets/scss/style.scss";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 function App() {
-  return <div className="App">Halo Dodi</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import Button from "elements/Button";
 export default class landingPage extends Component {
   render() {
-    return <div></div>;
+    return (
+      <>
+        <Header {...this.props}></Header>
+      </>
+    );
   }
 }
