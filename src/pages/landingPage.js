7e8de927ed9b75c8d74280d0bdab4f2c3c94c4c7
dodichakill <dodiherlangga91@gmail.com>
2020-11-17 07:29:36 -0800

    clr ep4.4

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
-// import Button from "elements/Button";
+import Hero from "parts/Hero";
+import landingPageJson from "json/landingPage.json";
 export default class landingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPageJson.hero} />
       </>
     );
   }
