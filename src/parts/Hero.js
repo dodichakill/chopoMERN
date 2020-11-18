12696e6eaf7327aea182a480c27575183d6a9299
dodichakill <dodiherlangga91@gmail.com>
2020-11-17 17:19:51 -0800

    clr ep4.5

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -17,7 +17,7 @@ export default function Hero(props) {
   return (
     <section className="container pt-4">
       <div className="row align-items-center">
-        <div className="col-auto pr-5" style={{ width: 422 }}>
+        <div className="col-auto pr-5" style={{ width: 530 }}>
           <h1 className="h2 font-weight-bold line-height-1 mb-3">
             Forget Busy Work, <br />
             Start Next Vacation
@@ -36,7 +36,7 @@ export default function Hero(props) {
           </Button>
 
           <div className="row mt-5">
-            <div className="col-auto">
+            <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 width="36"
                 height="36"
@@ -50,7 +50,7 @@ export default function Hero(props) {
                 </span>
               </h6>
             </div>
-            <div className="col-auto">
+            <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 width="36"
                 height="36"
@@ -58,7 +58,7 @@ export default function Hero(props) {
                 alt={`${props.data.treasures} Treasures`}
               />
               <h6 className="mt-3">
-                {props.data.travelers}{" "}
+                {props.data.treasures}{" "}
                 <span className="text-gray-500 font-weight-light">
                   Treasures
                 </span>
@@ -76,6 +76,23 @@ export default function Hero(props) {
                 <span className="text-gray-500 font-weight-light">Cities</span>
               </h6>
             </div>
+          </div>
+        </div>
+
+        <div className="col-6 pl-5">
+          <div style={{ width: 520, height: 410 }}>
+            <img
+              src={ImageHero}
+              alt="Room with couches"
+              className="img-fluid position-absolute"
+              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
+            />
+            <img
+              src={ImageHero_}
+              alt="Room with couches frame"
+              className="img-fluid position-absolute"
+              style={{ margin: "0 -15px -15px 0" }}
+            />
           </div>
         </div>
       </div>
