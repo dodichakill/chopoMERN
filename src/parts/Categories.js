b55cc438f6f611f53cd5d8aad15ade00e6c3d61d
dodichakill <dodiherlangga91@gmail.com>
2020-11-20 06:32:16 -0800

    clr ep6.3

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -1,19 +1,21 @@
 import React from "react";
 
+import Button from "elements/Button";
+
 export default function Categories({ data }) {
   return data.map((category, index1) => {
     return (
       <section className="container" key={`category-${index1}`}>
         <h4 className="mb-3 font-weight-medium">{category.name}</h4>
         <div className="container-grid">
-          {category.item.length === 0 ? (
+          {category.items.length === 0 ? (
             <div className="row">
               <div className="col-auto align-items-center">
                 There is no property at this category
               </div>
             </div>
           ) : (
-            category.item.map((item, index2) => {
+            category.items.map((item, index2) => {
               return (
                 <div
                   className="item column-3 row-1"
@@ -26,6 +28,25 @@ export default function Categories({ data }) {
                         <span className="font-weight-light">Choice</span>
                       </div>
                     )}
+                    <figure className="img-wrapper" style={{ height: 180 }}>
+                      <img
+                        src={item.imgUrl}
+                        alt={item.nama}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        href={`/properties/${item._id}`}
+                        className="streched-link d-block text-gray-800"
+                      >
+                        <h5 className="h4">{item.name}</h5>
+                      </Button>
+                      <span className="text-gray-500">
+                        {item.city}, {item.country}
+                      </span>
+                    </div>
                   </div>
                 </div>
               );
