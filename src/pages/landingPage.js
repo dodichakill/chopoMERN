e3de1032c4a2a986636f17fb00b96bffa5755280
dodichakill <dodiherlangga91@gmail.com>
2020-11-20 03:02:13 -0800

    clr ep5.8

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+
 import landingPageJson from "json/landingPage.json";
 export default class landingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPageJson.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPageJson.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPageJson.mostPicked}
+        />
       </>
     );
   }
