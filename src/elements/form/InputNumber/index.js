02a0ad659a1a526e5977f7ace78413cc01340c29
dodichakill <dodiherlangga91@gmail.com>
2021-01-08 01:59:08 -0800

    c 13.8 fix bug

--- src/elements/form/InputNumber/index.js
+++ src/elements/form/InputNumber/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import propTypes from "prop-types";
 
@@ -16,26 +16,16 @@ export default function Number(props) {
     isSuffixPlural,
   } = props;
 
-  const [InputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
-
   const onChange = (e) => {
     let value = String(e.target.value);
-    if (prefix) value = value.replace(prefix);
-    if (suffix) value = value.replace(suffix);
 
-    const patternNumeric = new RegExp("[0-9]*");
-    const isNumeric = patternNumeric.test(value);
-
-    if (isNumeric && +value <= max && +value >= min) {
+    if (+value <= max && +value >= min) {
       props.onChange({
         target: {
           name: name,
           value: +value,
         },
       });
-      setInputValue(
-        `${prefix}${value}${suffix}${isSuffixPlural && value > 1 ? "s" : ""}`
-      );
     }
   };
 
@@ -58,6 +48,7 @@ export default function Number(props) {
         },
       });
   };
+  /// TODO: tombol minus dan plus belum berfungsi
   return (
     <div className={["input-number mb-3", props.outerClassName].join(" ")}>
       <div className="input-group">
@@ -70,10 +61,12 @@ export default function Number(props) {
         <input
           min={min}
           max={max}
+          readOnly
           name={name}
-          pattern="[0-9]*"
           placeholder={placeholder ? placeholder : "0"}
-          value={String(InputValue)}
+          value={`${prefix}${value}${suffix}${
+            isSuffixPlural && value > 1 ? "s" : ""
+          }`}
           onChange={onChange}
           className="form-control"
         />
